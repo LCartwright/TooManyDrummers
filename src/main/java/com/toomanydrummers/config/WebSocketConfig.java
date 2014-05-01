2708f1140b1f0ad600f9f81eb986c57a4a1a834a
LCartwright <lc00167@surrey.ac.uk>
2014-05-01 19:35:24 +0100

    Websocket demo added

---
+++ src/main/java/com/toomanydrummers/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.toomanydrummers.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+	
+	public WebSocketConfig(){
+		super();
+		System.out.println("WEB SOCKET CONFIG LOADED");
+	}
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/hello").withSockJS();
+	}
+
+}
