5c922a640c41389983bf381bec62dade80dbecdc
JohnSharpe <john.sharpe@live.com>
2014-05-02 23:56:46 +0100

    Commented and formatted drums.js

--- src/main/webapp/resources/js/custom/drums.js
+++ src/main/webapp/resources/js/custom/drums.js
@@ -24,6 +24,11 @@ var audiochannels = new Array();
 // to the server every so often
 var mousePos;
 
+// How many milliseconds between each cursor location send.
+var mouseDelay = 200;
+// How many milliseconds between each cleanup of dead drumsticks.
+var cleanupDelay = 10000;
+
 // Change the UI to better represent the options available to a
 // connected/disconnected user.
 function setConnected(connected) {
@@ -48,82 +53,121 @@ function setConnected(connected) {
 			: 'hidden';
 }
 
+// This function is fired when the user tries to connect via a websocket to the
+// application.
+// TODO: This step should be automated with information from facebook.
+// function connect(facebookId) {
 function connect() {
 
+	// TODO: Make this element less of a nightmare to use by emptying it of the
+	// hint
+	// when clicked.
+	// TODO: Check for validity both here and on the serverside, even if
+	// facebook is supplying the info.
 	if (document.getElementById('enterUsername').value === "") {
 		alert('Please enter a username before connecting');
 	} else {
-		myId = document.getElementById('enterUsername').value;
-		// document.getElementById('chosenUsername').innerHTML = myId;
 
+		// TODO: As above, get this from another source.
+		myId = document.getElementById('enterUsername').value;
+
+		// Create a socket which looks for the 'hit' endpoint.
 		var socket = new SockJS('./hit');
+
+		// Prepare for full Stomp communication.
 		stompClient = Stomp.over(socket);
+
+		// Make the connection!
 		stompClient.connect({}, function(frame) {
+
+			// Let the user know we're connected!
 			setConnected(true);
-			// console.log('Connected: ' + frame);
+
+			// Register for hitreports so the user can hear other users playing
+			// and join in!
 			stompClient.subscribe('/topic/hitreports', function(drumhit) {
 				play(JSON.parse(drumhit.body).name);
 			});
 
+			// Subscribe for changes in the room's users so new users can join
+			// in
+			// and old users don't fill up the room with artifacts.
 			stompClient.subscribe('/topic/allusers', function(allUsers) {
 				refreshUsers(JSON.parse(allUsers.body));
 			});
 
-			// TODO: Replace 'names' with Facebook details
+			// TODO: Replace 'myId' with Facebook details
+			// TODO: Maybe we just need to send the ID and the server can pick
+			// up everything else.
+			// Let the Server know I've joined!
 			stompClient.send('/app/newuser', {}, JSON.stringify({
 				'id' : myId,
 				'firstName' : myId,
 				'lastName' : myId
 			}));
 
+			// Subscribe for updates on the cursor positions of other users.
 			stompClient.subscribe('/topic/motion', function(cursorPositions) {
 				moveDrumsticks(JSON.parse(cursorPositions.body));
 			});
 
+			// Listen for mouse movements
+			window.onmousemove = handleMouseMove;
+			// Fire off your location 5 times per second
+			// Increase this time for better performance
+			setInterval(sendMousePosition, mouseDelay);
+
+			// Clear dead users every 10 seconds.
+			setInterval(cleanDeadDrumsticks, cleanupDelay);
+
 		});
 	}
 
-	window.onmousemove = handleMouseMove;
-	setInterval(sendMousePosition, 200);
-	setInterval(cleanDeadDrumsticks, 10000);
-
 }
 
+// This is fired when the user hits 'Disconnect'
+// This would be preferable everytime but the user is likely to hit 'back' or
+// 'close' too
 function disconnect() {
 
-	// TODO Rather than sending a message, it would be better to react to
-	// disconnections on the serverside
+	// TODO Rather than sending a message, it would be much better to react to
+	// disconnections on the serverside...
 	stompClient.send('/app/finished', {}, JSON.stringify({
 		'id' : myId,
 		'firstName' : null,
 		'lastName' : null
 	}));
 
+	// Cleanly disconnect and inform the user.
 	stompClient.disconnect();
 	setConnected(false);
-	// console.log("Disconnected");
 }
 
+// Lets the server know that the bass drum has been hit!
 function hitBass() {
 	stompClient.send("/app/hit", {}, JSON.stringify({
 		'name' : 'bass'
 	}));
 }
 
+//Lets the server know that the tomtom has been hit!
 function hitTomTom() {
 	stompClient.send("/app/hit", {}, JSON.stringify({
 		'name' : 'tomtom'
 	}));
 }
 
+//Lets the server know that the snare has been hit!
 function hitSnare() {
 	stompClient.send("/app/hit", {}, JSON.stringify({
 		'name' : 'snare'
 	}));
 }
 
+// Play a sound!
 function play(message) {
-
+	
+	// This will (must) be the id of the appropriate <audio /> element 
 	var audio = message + 'sound';
 
 	// Modified version of the code available at this address:
@@ -148,24 +192,37 @@ function play(message) {
 
 }
 
-// Users is a comma-delimited string
+// Called whenever a user joins or leaves.
 function refreshUsers(users) {
+	
+	// TODO: Do something better with the list of connected users than just print them.
+	// TODO: Move this into the loop so this client's name is not displayed.
 	document.getElementById('connectedUsers').innerHTML = users;
-
+	
+	// Get the array of drumsticks. One is needed to track each user's mouse movements.
 	var drumsticks = document.getElementById('drumsticks').childNodes;
 
+	// Get all the ids provided
 	var allIds = users.split(",");
+	
+	// Clear the ids array - we have more recent information.
+	// TODO: Is there a cheaper clear() that can be used?
 	ids = new Array();
 
-	// Filter out my own username.
+	// For all users
 	for (var i = 0; i < allIds.length; i++) {
+		
+		// Filter out my own username and the empty value at the end
 		if (allIds[i] != myId && allIds[i] != "") {
+			
+			// Add it to the array of connected users!
 			ids.push(allIds[i]);
+			
 			// Ensure this id has a drumstick, else create one
-
 			var noDrumstick = true;
 
 			for (var j = 0; j < drumsticks.length; j++) {
+				// If we can find a drumstick for this user, we're done
 				if (drumsticks[j].getAttribute("id") == allIds[i]) {
 					noDrumstick = false;
 					break;
@@ -174,13 +231,20 @@ function refreshUsers(users) {
 
 			if (noDrumstick) {
 
+				// Create a new image
 				var newChild = document.createElement("img");
+				// Make it display the correct image
 				newChild
 						.setAttribute("src",
 								"${pageContext.request.contextPath}/resources/images/drumstick.png");
+				// Give it the appropriate id so it can be found by other methods
 				newChild.setAttribute("id", allIds[i]);
+				
+				// TODO: Find a better way of scaling down
 				newChild.setAttribute("width", "80");
 				newChild.setAttribute("width", "50");
+				
+				// Needed for positions to work
 				newChild.setAttribute("style", "position:absolute;");
 
 				document.getElementById('drumsticks').appendChild(newChild);
@@ -192,7 +256,7 @@ function refreshUsers(users) {
 
 }
 
-// SLOOOW, find a more efficient way of doing this...
+
 // This method is NOT responsible for creating/removing drumsticks.
 function moveDrumsticks(positions) {
 
@@ -205,6 +269,7 @@ function moveDrumsticks(positions) {
 
 		var currentId = positions[i].id;
 
+		// Filter out my own id.
 		if (currentId == myId) {
 			continue;
 		}
@@ -235,14 +300,23 @@ function moveDrumsticks(positions) {
 
 function handleMouseMove(event) {
 	event = event || window.event; // IE
-	// Consider nullifying mousePos if it hasn't changed
+	
+	// TODO: Consider nullifying mousePos if it hasn't changed
 	// so sendMousePosition() won't send an update
+	// if (mousePos.x == event.clientX && mousePos.y == event.clientY) {
+	// 	  mousePos = null;
+	// } else {
+	
+	// Update mousePos
 	mousePos = {
 		y : event.clientY,
 		x : event.clientX
 	};
+	
+	// }
 }
 
+// Fire off this client's mouse location to the server
 function sendMousePosition() {
 	if (mousePos) {
 		stompClient.send('/app/motion', {}, JSON.stringify({
@@ -253,10 +327,13 @@ function sendMousePosition() {
 	}
 }
 
+// Every now and then, delete the drumsticks belonging to disconnected users
+// both to remove clutter and free up resources.
 function cleanDeadDrumsticks() {
 
 	var drumsticks = document.getElementById('drumsticks').childNodes;
 
+	// For every drumstick image
 	for (var i = 0; i < drumsticks.length; i++) {
 
 		var drumstickMatchesDeadConnection = true;
@@ -270,20 +347,32 @@ function cleanDeadDrumsticks() {
 			}
 
 		}
-
+		
+		// If this is still true, delete the node!
 		if (drumstickMatchesDeadConnection) {
 			document.getElementById('drumsticks').removeChild(drumsticks[i]);
-			i--;
+			// TODO: I'm not sure we need to step back.
+			//i--;
 		}
 
 	}
 
 }
 
+// Stuff to do as soon as everything has loaded
 function initialize() {
 
+	// Make sure the user is not initially connected
 	setConnected(false);
 
+	// Modified version of the code available at this address:
+	// http://www.storiesinflight.com/html5/audio.html
+	// Updated: October 31, 2010
+	// Implementation of Rotating Audio Channels
+	// If a request is made for an audio element to play a file while
+	// already playing one, the request will be dropped.
+	// This array contains channel_max audio channels and the earliest
+	// 'free' channel is used to handle a sound request.
 	for (var a = 0; a < audio_channel_max; a++) {
 		audiochannels[a] = new Array();
 		audiochannels[a]['channel'] = new Audio();
